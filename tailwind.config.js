457dbd33344f6b13d73b622ab049b92ed20cc71c
Claire <c.tregunna1980@gmail.com>
2025-02-04 16:16:03 +0000

    initla files inc tailwind v4

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}",
+            "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        }
+       },
+    plugins: [
+      require('flowbite/plugin')
+    ],
+  }
+}
+
